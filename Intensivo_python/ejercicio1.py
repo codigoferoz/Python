6c6e2fee5acd1a239752ed28cd4fb46f074e108d
codigoferoz <adrian-lopez@live.com.ar>
2022-01-05 02:19:39 -0300

    Create ejercicio1.py
    Ejercicio 1
    
    
    Enunciado: Imprime todos los ficheros existentes en tu carpeta de Descargas.
    
    
    Objetivo:
    
    
    - Aprender a utilizar librerías sencillas (en este caso, os) y sus funciones. 
    - Aprender a utilizar los bucles y los condicionales. 
    
    El algoritmo del ejercicio es el siguiente:
    
    
    - Obtén todos los ficheros y directorios de un directorio en concreto. Para ello necesitas una función existente en la librería os (Sistema Operativo) de Python.
    - Recorre todos los resultados obtenidos por la función anterior. Lo puedes hacer, por ejemplo, con un bucle for.
    - Imprime por pantalla solo aquellos resultados que sean ficheros (para ello también necesitas una función existente en os. 
    
    Ampliación:
    
    
    - Lista los tamaños de los ficheros en formato humano.
    - Recorre de manera recursiva todos los directorios desde tu carpeta personal y muestra los ficheros de cada directorio y su tamaño.

---
+++ Intensivo_python/ejercicio1.py
@@ -0,0 +1,33 @@
+import os
+import humanize
+
+# Obtiene todos los ficheros
+
+search_dir = os.listdir('/home/ken/Descargas')
+
+print(search_dir)
+
+# Recorre todos los archivos
+
+downloads = '/home/ken/Descargas'
+
+with os.scandir(downloads) as all_content:
+    for content in all_content:
+        print(content.name)
+
+# Imprime en pantalla sólo los ficheros
+
+with os.scandir(downloads) as specific_content:
+    specific_content = [file.name for file in specific_content if file.is_file()]
+
+print(specific_content)
+
+# Lista los tamaños de los ficheros en formato human friendly
+
+all_files = filter(lambda x: os.path.isfile(os.path.join(downloads, x)), os.listdir(downloads))
+
+files_sizes = [(content, os.stat(os.path.join(downloads, content)).st_size) for content in all_files]
+
+for content, size in files_sizes:
+    print(content, ': ', (humanize.naturalsize(size)))
+
